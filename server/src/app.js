8425d722833089ebfd8dd7713cb3316ea565efcd
unknown <skullet-on@mail.ru>
2019-08-26 12:39:37 +0300

    Fixed eslint errors

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const app = express();
 const dotenv = require('dotenv');
+
 dotenv.config();
 const port = process.env.PORT || 4000;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+app.listen(port, () => {
 });
